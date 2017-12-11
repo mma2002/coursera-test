d0867836e4261b5f53f31ce7bbb53093924b71c6
Marat <mma2002@mail.ru>
2017-12-11 23:58:47 +0600

    done module2

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,29 +1,6 @@
 (function () {
 'use strict';
 
-var shoppingListToBuy = [
-  {
-    name: "Milk",
-    quantity: "2"
-  },
-  {
-    name: "Donuts",
-    quantity: "200"
-  },
-  {
-    name: "Cookies",
-    quantity: "300"
-  },
-  {
-    name: "Chocolate",
-    quantity: "5"
-  },
-  {
-    name: "Waters",
-    quantity: "15"
-  }
-];
-
 angular.module('ShoppingListCheckOff', [])
 .controller('ToBuyController', ToBuyController)
 .controller('AlreadyBoughtController', AlreadyBoughtController)
@@ -35,11 +12,71 @@ function ToBuyController(ShoppingListCheckOffService) {
 
   toBuyList.items = ShoppingListCheckOffService.getItems();
 
-  toBuyList.buyItem = function (itemIndex) {
-    ShoppingListCheckOffService.buyItem(itemIndex);
+  toBuyList.buy = function (itemIndex) {
+    // console.log('itemIndex:'+toBuyList.items[0].name);
+    // console.log('name:'+toBuyList[itemIndex].quantity);
+    ShoppingListCheckOffService.addItem(toBuyList.items[itemIndex].name, toBuyList.items[itemIndex].quantity);
+    ShoppingListCheckOffService.removeItem(itemIndex);
   };
 }
 
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtList = this;
+
+  boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  // var items = [];
+  var items = [
+    {
+      name: "Milk",
+      quantity: "2"
+    },
+    {
+      name: "Donuts",
+      quantity: "200"
+    },
+    {
+      name: "Cookies",
+      quantity: "300"
+    },
+    {
+      name: "Chocolate",
+      quantity: "5"
+    },
+    {
+      name: "Waters",
+      quantity: "15"
+    }
+  ];
+
+  var boughtItems = [];
+
+  service.addItem = function (itemName, quantity) {
+    var item = {
+      name: itemName,
+      quantity: quantity
+    };
+    boughtItems.push(item);
+  };
+
+  service.removeItem = function (itemIdex) {
+    items.splice(itemIdex, 1);
+  };
+
+  service.getItems = function () {
+    return items;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+}
 
 })();
