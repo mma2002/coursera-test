c2f44a4eadc6ad6da713815ffcab300d2f302d04
mma2002 <m.marat1978@gmail.com>
2017-12-11 20:54:20 +0600

    add initial module2 code

---
+++ module2-solution/app.js
@@ -0,0 +1,45 @@
+(function () {
+'use strict';
+
+var shoppingListToBuy = [
+  {
+    name: "Milk",
+    quantity: "2"
+  },
+  {
+    name: "Donuts",
+    quantity: "200"
+  },
+  {
+    name: "Cookies",
+    quantity: "300"
+  },
+  {
+    name: "Chocolate",
+    quantity: "5"
+  },
+  {
+    name: "Waters",
+    quantity: "15"
+  }
+];
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuyList = this;
+
+  toBuyList.items = ShoppingListCheckOffService.getItems();
+
+  toBuyList.buyItem = function (itemIndex) {
+    ShoppingListCheckOffService.buyItem(itemIndex);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+
+})();
